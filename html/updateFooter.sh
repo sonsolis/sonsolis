65bafef9a07b3f6729a91a9ce2b35d9815b25472
saiyajinsolis <saiyajinsolis@gmail.com>
2025-07-22 19:28:57 -0400

    Small adjustments to the footers Contact Us section.

---
+++ html/updateFooter.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+HTML_DIR="./"
+
+
+
+for file in "$HTML_DIR"/*.html; do
+	# 1. Get the Starting Line Number
+	start_num=$(grep -n "Quick Links" $file | cut -d: -f1)
+
+	# 2. Config
+	steps=8
+
+	# 3. Build Array
+	numbers=()
+	for ((i=0; i<$steps; i++)); do
+		numbers[$i]=$(( start_num + i))
+	done
+
+	echo "Deleting Line after Quick Links in $file..."
+	for line in "${numbers[@]}"; do
+		sed -i '${line}d' "$file"
+	done
+done
+
+echo "Lines Deleted!"
